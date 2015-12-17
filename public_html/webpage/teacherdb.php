90c70b4e855b0186fafeb2245d505a85f79f8484
JackTheBOSSYT <jackroybal1810@gmail.com>
2015-12-17 10:40:40 -0800

    first upload

---
+++ public_html/webpage/teacherdb.php
@@ -0,0 +1,7 @@
+<?php 
+// These variables define the connection for the Teacher database 
+     $username = "plumkmda"; 
+     $password = "roLl8warn@"; 
+     $host = "pav-dbs-0103"; 
+     $dbname = "pluma001"; 
+?> 
